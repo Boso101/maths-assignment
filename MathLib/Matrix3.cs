c7ce0648369e115a08f897b195098fd10f8073c0
Boso101 <bluestarz_5000@hotmail.com>
2021-03-17 18:19:00 +1030

    RotateX Matrix3

--- MathLib/Matrix3.cs
+++ MathLib/Matrix3.cs
@@ -1,4 +1,6 @@
-﻿namespace MathClasses
+﻿using System;
+
+namespace MathClasses
 {
     public class Matrix3
     {
@@ -85,8 +87,8 @@
         public void SetRotateX(float radians)
         {
             m1 = 1; m4 = 0; m7 = 0;
-            m2 = 0; m5 = 1; m8 = 0;
-            m3 = 0; m6 = 0; m9 = 1;
+            m2 = 0; m5 = (float)Math.Cos(radians); m8 = -1 * (float)Math.Sin(radians);
+            m3 = 0; m6 = (float)Math.Sin(radians); m9 = (float)Math.Cos(radians);
         }
 
         /// <summary>
