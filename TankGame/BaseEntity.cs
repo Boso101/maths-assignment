bc66edadfc200422ac2c3a511ed226535dd0d792
Boso101 <s210130@students.aie.edu.au>
2021-03-17 15:10:29 +1030

    Create BaseEntity.cs

---
+++ TankGame/BaseEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClasses;
+
+namespace Project2D
+{
+    public abstract class BaseEntity
+    {
+
+        protected Vector2 position;
+        protected BaseEntity parent;
+        protected List<BaseEntity> children;
+
+
+
+        public BaseEntity()
+        {
+            position = new Vector2(0, 0);
+            parent = null;
+
+        }
+
+        public BaseEntity(Vector2 spawnPosition, BaseEntity parent = null)
+        {
+            position = spawnPosition;
+            this.parent = parent;
+            
+        }
+        
+    } 
+}
